f8e54d5af86823c50b85a4927171eefd71b4d5ef
Sherlock Yao <yaosherlock@gmail.com>
2014-07-01 17:48:04 +0800

    add support for looply read line for cmd

--- src/yvetee/core.clj
+++ src/yvetee/core.clj
@@ -1,7 +1,23 @@
 (ns yvetee.core
   (:gen-class))
 
+(defn exec-cmd
+  [cmd]
+  (if (= cmd "exit")
+    true
+    (do
+      (println (str cmd " executed"))
+      false)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "The main entry of the app."
   [& args]
-  (println "Hello, World!"))
+  (loop [cmd "help"]
+    (if (exec-cmd cmd)
+      (println "Bye Bye")
+      (recur (read-line)))))
+
+;(loop [cmd (read-line)]
+;  (if (== cmd "exit")
+;    (println "Bye Bye")
+;    (recur (read-line)))
